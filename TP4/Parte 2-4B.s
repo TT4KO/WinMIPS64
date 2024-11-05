3645f0f9b3657b5ad5076a4a957750d6cd8125a0
TT4KO <MateoQS4@hotmail.com>
2024-11-05 15:51:52 -0300

    Create Parte 2-4B.s

---
+++ TP4/Parte 2-4B.s
@@ -0,0 +1,34 @@
+.data
+control: .word 0x10000
+data: .word 0x10008
+color: .byte 255, 0, 0, 0
+equis: .byte 0
+igriega: .byte 0
+
+.code
+ld $s0, control($zero)
+ld $s1, data($zero)
+daddi $t5, $zero, 5
+daddi $s2, $zero, 8
+
+
+sb $s2, 0($s0)
+lbu $t1, 0($s1)
+sb $t1, equis($zero)
+
+sb $s2, 0($s0)
+lbu $t2, 0($s1)
+sb $t2, igriega($zero)
+
+lbu $t0, equis($zero)
+sb $t0, 5($s1)
+lbu $t0, igriega($zero)
+sb $t0, 4($s1)
+
+loop:
+lwu $t3, color($zero)
+sw $t3, 0($s1)
+daddi $t3, $t3, -1
+sd $t5, 0($s0)
+j loop
+halt 
